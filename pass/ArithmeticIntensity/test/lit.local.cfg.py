378332e697b84d07714f4eac84a107737181fa8c
Simon Waters <simon@kernelize.ai>
2026-05-12 17:09:44 -0500

    [ArithmeticIntensity] Use AffineExpr to canonicalize the equations - added pytests

--- pass/ArithmeticIntensity/test/lit.local.cfg.py
+++ pass/ArithmeticIntensity/test/lit.local.cfg.py
@@ -4,7 +4,7 @@ import sys
 # Extend the test environment from `lit-test/lit.cfg.py`: point `triton-opt` at the shared library for this pass. In the
 # future, this should calculate an OS-agnostic path, possibly using the CMake target (TODO).
 config.environment["TRITON_PASS_PLUGIN_PATH"] = os.path.join(
-    config.triton_ext_binary_dir, "lib", "libcompute_density.so")
+    config.triton_ext_binary_dir, "lib", "libarithmetic_intensity.so")
 print(
     f"ENV: "
     f"LD_LIBRARY_PATH={config.environment['LD_LIBRARY_PATH']} "
